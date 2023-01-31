3b35ae87f81ee6cad315c204b887e0c3e4d645f5
sakklavut <sakklavut23395@gmail.com>
2023-01-31 09:34:29 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row mr-0 ml-0">
+		<div class="col-12 col-md">
+			<small class="d-block mb-3 text-muted">&copy; 2017-2021</small>
+		</div>
+		<div class="col-6 col-md">
+			<h5> Footer 1 </h5>
+			<ul class="list-unstyled text-small">
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+			</ul>
+		</div>
+		<div class="col-6 col-md">
+			<h5> Footer 2 </h5>
+			<ul class="list-unstyled text-small">
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+			</ul>
+		</div>
+		<div class="col-6 col-md">
+			<h5> Footer 3 </h5>
+			<ul class="list-unstyled text-small">
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+				<li><a class="text-muted" href="#"> Footer </a></li>
+			</ul>
+		</div>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include('comtop.php'); ?>
+<?php include('header.php'); ?>
+
+
+<div class="container-fluid">
+	<div class="container">
+		<h1 class="mt-5"> Hello, New Project !!! </h1>
+		<div class="">
+			Hi
+		</div>
+	</div>
+</div>
+
+
+
+<?php include('footer.php'); ?>
+<?php include('combottom.php'); ?>
+		
