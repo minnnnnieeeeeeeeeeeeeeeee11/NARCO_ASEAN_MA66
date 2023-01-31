8f769211dfbec7ca207c62b395c0a13a8e70cf3a
Sakklavut Imsamai <92006247+sakklavut@users.noreply.github.com>
2023-01-31 09:37:20 +0700

    Update combottom.php

--- combottom.php
+++ combottom.php
@@ -2,6 +2,5 @@
 		<script src="assets/js/jquery.js"></script>
 		<script src="assets/js/bootstrap.js"></script>
 		<script src="assets/js/popper.js"></script>
-		<script src="assets/js/bootstrap.bundle.js"></script>
 	</body>
 </html>
